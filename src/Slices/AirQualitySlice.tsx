a2660a0e08f56b226484b5779351a725a96be5fa
Madhu Baloni <madhubaloni@CELSYSNCR000032.local>
2025-07-11 17:35:13 +0530

    updated

--- src/Slices/AirQualitySlice.tsx
+++ src/Slices/AirQualitySlice.tsx
@@ -1,5 +1,5 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AirQualityData, WeatherState } from "../Types/Type";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
